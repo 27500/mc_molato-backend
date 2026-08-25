eb7a9f5123aea9a7cf5284f32958c17875abf0c4
27500 <blessingmingenge@gmail.com>
2026-08-25 19:56:24 +0100

    arranger la taille de la photo dans la page admin

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ connectDB();
 
 const app = express();
 
-// Middlewares
+// Middlewares avec augmentation de la limite de taille pour les images Base64
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Rendre le dossier "uploads" accessible publiquement
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
@@ -21,12 +22,10 @@ app.use('/api/products', require('./routes/productRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/orders', require('./routes/orderRoutes'));
 
-// Route de test pour vérifier l'état du serveur
 app.get('/', (req, res) => {
   res.send('API Mc Molato en marche...');
 });
 
-// LANCEMENT DU SERVEUR POUR RENDER ET LOCAL
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
